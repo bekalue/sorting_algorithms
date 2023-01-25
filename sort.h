5aaa01ff1dee2cfb3ef787eaab4f8a504171c922
Bekalu Geta <bekalu2054@gmail.com>
2023-01-25 16:23:09 +0300

    .

--- sort.h
+++ sort.h
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 
 /**
- * stuct listint_s - Doubly linked list node.
+ * struct listint_s - Doubly linked list node.
  *
  * @n: Integer stored in the node.
  * @prev: Pointer to the previous element of the list.
@@ -20,43 +20,44 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);  /* Source File: print_list.c */
-void print_array(const int *array, size_t size); /* Source File: print_array.c */
+/* Source File: print_list.c */
+void print_list(const listint_t *list);
+/* Source File: print_array.c */
+void print_array(const int *array, size_t size);
 
-/*    BUBBLE SORT     */
-void bubble_sort(int *array, size_t size); /* Source File: 0-bubble_sort.c */
+/* BUBBLE SORT || Source File: 0-bubble_sort.c */
+void bubble_sort(int *array, size_t size);
 
-/*   INSERTION SORT  */
-void insertion_sort_list(listint_t **list); /* Source File: 1-insertion_sort_list.c */
+/* INSERTION SORT || Source File: 1-insertion_sort_list.c*/
+void insertion_sort_list(listint_t **list);
 
-/*   SELECTION SORT */
-void selection_sort(int *array, size_t size); /* Source File: 2-selection_sort.c */
+/* SELECTION SORT || Source File: 2-selection_sort.c */
+void selection_sort(int *array, size_t size);
 
-/*   QUICK SORT      */
-void quick_sort(int *array, size_t size); /*source File: 3-quick_sort.c */
+/* QUICK SORT || source File: 3-quick_sort.c  */
+void quick_sort(int *array, size_t size);
 
-/*   SHELL SORT USING KNUTH SEQUENCE */
-void shell_sort(int *array, size_t size); /* source file: 100-shell_sort.c */
+/* SHELL SORT USING KNUTH SEQUENCE || source file: 100-shell_sort.c*/
+void shell_sort(int *array, size_t size);
 
-/*   COCKTAIL SHAKER SORT  */
-void cocktail_sort_list(listint_t **list); /* source file: 101-cocktail_sort_list.c*/
+/* COCKTAIL SHAKER SORT || source file: 101-cocktail_sort_list.c*/
+void cocktail_sort_list(listint_t **list);
 
-/*   COUNTING SORT  */
-void counting_sort(int *array, size_t size); /* source file: 102-counting_sort.c */
+/* COUNTING SORT || source file: 102-counting_sort.c */
+void counting_sort(int *array, size_t size);
 
-/*   MERGE SORT    */
-void merge_sort(int *array, size_t size);  /* source file: 103-merge_sort.c */
+/* MERGE SORT || source file: 103-merge_sort.c */
+void merge_sort(int *array, size_t size);
 
-/*   HEAP SORT   */
-void heap_sort(int *array, size_t size);  /* SOURCE FILE: 104-heap_sort.c */
+/* HEAP SORT || SOURCE FILE: 104-heap_sort.c  */
+void heap_sort(int *array, size_t size);
 
-/*  RADIX SORT  */
-void radix_sort(int *array, size_t size); /* SOURCE FILE: 105-radix_sort.c */
+/* RADIX SORT || SOURCE FILE: 105-radix_sort.c */
+void radix_sort(int *array, size_t size);
 
-/*  BITONIC SORT */
-void bitonic_sort(int *array, size_t size); /* SOURCE FILE: 106-bitonic_sort.c */
+/* BITONIC SORT || SOURCE FILE: 106-bitonic_sort.c*/
+void bitonic_sort(int *array, size_t size);
 
-/*  QUICK SORT USING HOARE PARTITION SCHEME */
-void quick_sort_hoare(int *array, size_t size); /* SOURCE FILE: 107-quick_sort_hoare.c */
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
